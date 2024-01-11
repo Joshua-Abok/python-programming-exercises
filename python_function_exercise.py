776e916c667a1464ae12ebd08b5507f4dc721106
Joshua Abok <nyajuaya.j.a@gmail.com>
2024-01-11 17:58:09 +0300

    Python function exercise

---
+++ python_function_exercise.py
@@ -0,0 +1,143 @@
+"""
+
+Question 1:
+-----------
+
+Write a Python function "power_of_two" that takes an integer as input and returns its square. 
+Then, use this function to calculate the square of 78."""
+# Write your code here 
+
+
+
+
+
+
+
+
+
+
+"""
+Question 2:
+------------
+ Write a function "get_absolute_value" that takes a number as input and returns its absolute value (i.e., the value without the negative sign). 
+ Test the function with both positive and negative numbers."""
+
+# Write your code here 
+
+
+
+
+
+
+
+
+
+"""
+Question 3:
+-----------
+
+Create a function "reverse_string" that takes a string as input and returns the string reversed. 
+Test this function with:
+             "hello" string -> Output: "olleh". """
+
+# Write your code here 
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 4:
+----------
+
+Write a function "filter_even_numbers" that takes a list of integers as input and uses a lambda function to filter out the even numbers. 
+Call this function with an example list:
+            -> list_int = [23, 45, 4, 78, 90]. """
+
+# Write your code here 
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 5:
+----------
+
+Develop a function "count_vowels" that takes a string as input and uses a lambda function to count the number of vowels in the string. 
+Test this function with string: 
+     -> string_val = money."""
+
+# Write your code here 
+
+
+
+
+
+
+
+
+
+
+"""
+Question 6:
+-----------
+
+Write a function "remove_duplicates" that takes a list and uses a lambda function to remove duplicate elements. 
+Test this function with:
+            num_values = [3.0, 4, 3.0, 56, 7, 4] """
+
+# Write your code here 
+
+
+
+
+
+
+
+
+
+
+"""
+Question 7: 
+-----------
+
+Write a "function is_even" that checks if a given number is even. 
+The function should return True if the number is even and False if it's odd. 
+Test the function either 9 and 98"""
+
+# Write your code here 
+
+
+
+
+
+
+
+
+
+
+"""
+Question 8: 
+-----------
+
+Create a function "add_to_element" that takes a list and a value as input and adds that value to every element in the list. 
+For example, if you call add_to_element([1, 2, 3], 5), it should return [6, 7, 8].
+"""
+
+# write your code here
