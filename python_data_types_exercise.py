ae8d4b8a61bed17761c4f9e8315cba3e388fd3a5
Joshua Abok <nyajuaya.j.a@gmail.com>
2024-01-20 18:37:47 +0300

    Python Data Types exercise

---
+++ python_data_types_exercise.py
@@ -0,0 +1,248 @@
+"""
+
+Question 1:
+----------
+
+Write a Python program to add the following data types and get the result as an integer type. """
+
+var1 = 10         # integer
+var2 = 3.14       # float data type
+result = ...
+print(result)
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Question 2:
+-----------
+
+Write a Python program to concatenate the two string types."""
+
+string1 = "Hello"
+string2 = "World"
+
+        # String contatenation + 
+concat_string = ...
+print(concat_string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Question 3:
+----------
+
+Maria bought 96 toys priced equally for €12960. The amount of $1015 is still left with her. Find the cost of each toy and the amount she had. 
+Write a Python program to calculate the cost of each toy.""" 
+
+# write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 4:
+----------
+
+Write a Python program to split the words in the sentence. And then print the length of the of the words created."""
+
+sentence = "Python is an awesome language"
+words = ...
+
+print("len of sentence: ", len(words))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 5:
+----------
+
+Change the string to uppercase """
+
+string = "Hello, World!"
+UC_string = ...
+print(UC_string)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 6:
+----------
+replace the world with "Python" and display the result. """
+
+string = "Hello, World!"
+string = ...
+print(string)
+
+# write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 7: 
+----------
+
+pick the "mango" and value 3 for this question. """
+
+fruits = ("apple", "dates", "mango", [1, 2, 3])
+
+# write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Question 8:
+----------
+
+pick "value2" from the dictionary. """
+
+dict_values = {"key1": "value1", "key2": "value2"}
+
+# write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 9:
+-----------
+
+reassign "mango" with "dates". """
+
+d_nested = {1: {'innerKey': [23, 45, "mango"]}}
+
+#write your code here
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+Question 10:
+-----------
+
+output the intersection of list1 and list2. 
+Output -> {3, 4, 5} """
+
+list1 = [1, 2, 3, 4, 5]
+list2 = [3, 4, 5, 6, 7]
+
+#write your code here
+
+
+
+
+
+
+
+
+
+
+
+"""
+
+Question 11: 
+----------
+
+Design a Python program that takes 39 degrees Celsius temperature and converts it to Fahrenheit. 
+The formula for conversion is F = (C * 9/5) + 32. """
+
+# Write your code here 
+
