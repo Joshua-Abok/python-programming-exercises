6442d1aec60ab63d700470e1f93d692a618ba530
Joshua Abok <nyajuaya.j.a@gmail.com>
2024-01-31 20:00:33 +0300

    Branching in Python

---
+++ python_branching_exercise.py
@@ -0,0 +1,204 @@
+'''
+>>> GUIDES <<<<
+  -> No pressure, if you are not able to 
+     complete all the tasks. We can always pick up where you left off in our session. 
+  -> attempt atleast one question between question 4 and 6
+  -> Get to ask questions where you are stuck
+  
+
+
+Question 1:
+----------
+
+Create two sets of numbers and find their union, intersection, and the difference between them.
+Hint: this will need set datatype
+      check our last question on previous exercise'''
+
+# write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Question 2:
+----------
+
+Write a Python program with a list of numbers and calculates the sum of all the numbers in the list. '''
+
+# write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Question 3: 
+----------
+
+Combine logical operators to check if a number x is between 10 and 20 or between 30 and 40.
+'''
+
+# write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Question 4:
+----------
+
+Student Grading System:
+-----------------------
+
+You are creating a program to calculate and assign your grades based on your scores. The grading criteria are as follows:
+
+A: 90-100
+B: 80-89
+C: 70-79
+D: 60-69
+F: Below 60
+You get a score of 93%. 
+Write a Python program that will take your score as input and assign it the appropriate grade.
+Hint: Use if-elif-else statements.
+      Also let's give your score a variable name, right :)
+      my_score = 93'''
+
+# write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Question 5:
+----------
+
+ Income Tax Calculation:
+ -----------------------
+
+You are building a tax calculation program to help work around your taxes easily :)
+The tax rules are as follows:
+
+If your annual income is less than or equal to €30,000, your are exempted from tax :)
+If your income is between €30,001 and €50,000, you pay 10% tax.
+If your income is between €50,001 and €100,000, you pay 20% tax.
+If your income is over €100,000, you pay 30% tax.
+Write a Python program that can help you take your income. And then calculate for you the tax to pay. '''
+
+# write your code here!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+Question 6:
+-----------
+
+    LOAN ELIGIBILITY
+    ----------------
+
+Now you have worked on your taxes. You wanna know your eligibility for this loan that you've always wanted :)
+You thought of designing a loan approval system that you can cross reference with your lending institution. 
+The rules are as follows for a loan:
+
+Your credit score should be above 650.
+If the credit score is below 650, the annual income (ai) should be at least €70,000 for approval.  -> < 650 or ai >= 70k 
+The loan amount should be less than or equal to 2 times the annual income.
+Write a Python program that takes your credit score (which unfortunately is 500), annual income (reference previous question for this), and requested loan amount (have any value here) 
+and determines whether you are eligible for a loan 
+
+Hint: start by creating variables for the different values;
+      Use if-elif-else statements.'''
+
+# write your code here!
+
